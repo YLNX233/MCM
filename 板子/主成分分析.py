b8b2a6df65f850ed7c06bd6ec4bbcd91d85ffd07
ZCH233 <89012028+MOYFbeta@users.noreply.github.com>
2022-02-17 18:22:07 +0800

    .

--- 板子/主成分分析.py
+++ 板子/主成分分析.py
@@ -8,9 +8,8 @@ from sklearn.datasets import make_blobs
 # X为样本特征，Y为样本簇类别， 共1000个样本，每个样本3个特征，共4个簇
 X, y = make_blobs(n_samples=10000, n_features=3, centers=[[3,3, 3], [0,0,0], [1,1,1], [2,2,2]], cluster_std=[0.2, 0.1, 0.2, 0.2], 
                   random_state =9)
-pca = PCA(n_components=2)
+pca = PCA(n_components=2)# 注：当n_components<1 时代表主成分的方差和所占的最小比例阈值，此时将会自动决定维数
 pca.fit(X)
-
 
 X_new = pca.transform(X)
 #plt.scatter(X[:, 0], X[:, 1], X[:, 2],marker='o')
