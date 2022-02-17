586a5c44255c2926334d3e1970941006d9431fb2
ZCH233 <89012028+MOYFbeta@users.noreply.github.com>
2022-02-17 21:05:39 +0800

    给流场新加了功能，上传了效果图sample（在readme里）

--- 板子/绘图板子/pypolt.py
+++ 板子/绘图板子/pypolt.py
@@ -4,8 +4,12 @@ import numpy as np
 from math import sin
 from plotdf import plotdf
 
+NY = 100
+NX = 100
 xmin,xmax = [-10,10]
 ymin,ymax = [-14,14]
+dy = (ymax -ymin )/(NY-1.)
+dx = (xmax -xmin)/(NX-1.)
 
 def f(x,g=1,l=1,m=1,b=1):
     return np.array([x[1],-g*sin(x[0])/l-b*x[1]/m/l])
@@ -13,6 +17,8 @@ def f(x,g=1,l=1,m=1,b=1):
 # print(plt.style.available)
 # 双变量微分方程相图
 plt.style.use('seaborn-deep')# 主题可以改一改，虽然还是有点丑
+
+# 特定解
 plotdf(f, # 微分方程。定义如上，所有自变量打包在一个x里面
     np.array([xmin,xmax]), # [xmin,xmax]
     np.array([ymin,ymax]),# [ymin,ymax]
@@ -21,12 +27,17 @@ plotdf(f, # 微分方程。定义如上，所有自变量打包在一个x里面
     gridsteps= 0, # 梯度矢量场标箭头的密度
     nsteps=10000) # 计算一组解的总步数
 plt.legend(['a','b','c'])
-# 梯度矢量场还是另画更好看
-x, y = np.mgrid[xmin:xmax, ymin:ymax]
+
+# 梯度矢量场
+y = np.array([ ymin + float(i)*dy for i in range(NY)])
+x = np.array([ xmin + float(i)*dx for i in range(NX)])
+x, y = np.meshgrid( x, y, indexing = 'xy', sparse = False)
+
 u = y
 v = -9.8*np.sin(x)/0.5 - 0.05*x/0.3/0.5
 color = x + y # 颜色
-plt.quiver(x, y, u, v, color)
+plt.quiver(x, y, u, v, color) # 箭头
+plt.streamplot(x, y, u, v, color=color,linewidth=v/10) # 流
 
 plt.colorbar()
 
--- 板子/随机过程/MCMC.py
+++ 板子/随机过程/MCMC.py
@@ -6,26 +6,30 @@ from scipy import optimize
 import matplotlib.pyplot as plt
 
 time = np.random.rand(20)
-sleep_obs = np.random.randint(0,1)
+sleep_obs = np.random.rand(20)
+N_SAMPLES = 5000
 
-with pm.Model() as sleep_model:# 拟合一个logisit，参数alpha、beta
-
+with pm.Model() as sleep_model:
     # Create the alpha and beta parameters
-    # Assume a normal distribution
-    alpha=pm.Normal('alpha', mu=0.0, tau=0.05, testval=0.0)
-    beta=pm.Normal('beta', mu=0.0, tau=0.05, testval=0.0)
-
-    # The sleep probability is modeled as a logistic function
-    p=pm.Deterministic('p', 1. / (1. +tt.exp(beta * time + alpha)))
-
-    # Create the bernoulli parameter which uses observed data to inform the algorithm
-    observed=pm.Bernoulli('obs', p, observed=sleep_obs)
-
+    alpha = pm.Normal('alpha', mu=0.0, tau=0.01, testval=0.0)
+    beta = pm.Normal('beta', mu=0.0, tau=0.01, testval=0.0)
+    
+    # Create the probability from the logistic function
+    p = pm.Deterministic('p', 1. / (1. + tt.exp(beta * time + alpha)))
+    
+    # Create the bernoulli parameter which uses the observed dat
+    observed = pm.Bernoulli('obs', p, observed=sleep_obs)
+    
+    # Starting values are found through Maximum A Posterior estimation
+    # start = pm.find_MAP()
+    
     # Using Metropolis Hastings Sampling
-    step=pm.Metropolis()
+    step = pm.Metropolis()
+    
+    # Sample from the posterior using the sampling method
+    sleep_trace = pm.sample(N_SAMPLES, step=step, njobs=2)
 
-    # Draw the specified number of samples
-    sleep_trace=pm.sample(2000, step=step)
+    pm.traceplot(sleep_trace, ['alpha', 'beta'])
+    pm.autocorrplot(sleep_trace, ['alpha', 'beta']);
 
-pm.traceplot(sleep_trace, ['alpha', 'beta'])
-pm.autocorrplot(sleep_trace, ['alpha', 'beta'])
+plt.show()
