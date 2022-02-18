6a1a2a503f45641617823db9c878cb829672ec71
ZCH233 <89012028+MOYFbeta@users.noreply.github.com>
2022-02-18 22:23:13 +0800

    封装了用于读取信息的模块为一个class

--- codes/track.py
+++ codes/track.py
@@ -1,45 +1,58 @@
 import numpy as np
-import numpy.linalg as LA
 import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-import geotable as geo
 from codes.libs import *
-# %% 全局变量
+
 EARTH_R = 6371.393
 STEPS = 10000 # 地图中点的数目
 
-# %% 转换地图数据为千米
-def longitudeLatitude_2_xy(latitude,longitude):
-    latitude_km = (2 * np.pi * EARTH_R)/(360)
-    latitude -= latitude.mean() # 中心化
-    latitude = latitude*latitude_km
+class track:
+    '''
+    功能:计算赛道上关于路程的信息,并提供关于 dist 的查询
+    初始化:输入一个numpy array,其第一行、第二行、第三行分别是纬度、经度、高度
+    '''
+    def __init__(self,route) -> None:
+        self.y,self.x = self.longitudeLatitude_2_xy(route[0],route[1])
+        self.height = route[2]
+        self.n = self.x.size
+        self.traverse_dist = 0
+        self.d_list = [0]
+        self.coordinate_2_dist()
+        self.curvature = []
+        self.direction = []
 
-    longitude_km = (np.pi * EARTH_R * np.cos(latitude.mean()))/(180)
-    longitude -= longitude.mean()
-    longitude = longitude*longitude_km
+    def longitudeLatitude_2_xy(latitude,longitude):
+        latitude_km = (2 * np.pi * EARTH_R)/(360)
+        latitude -= latitude.mean() # 中心化
+        latitude = latitude*latitude_km
 
-    return latitude,longitude
+        longitude_km = (np.pi * EARTH_R * np.cos(latitude.mean()))/(180)
+        longitude -= longitude.mean()
+        longitude = longitude*longitude_km
 
+        return latitude,longitude
 
-# %% 计算出坡度、方向、曲率关于距离的函数
-def coordinate_2_dist(x,y):
-    n = x.size
-    dist = 0
-    for i in range(n):
+    def coordinate_2_dist(self):
+        for i in range(1,self.n):
+            self.traverse_dist += (self.x[i]-self.x[i-1])**2 + (self.y[i]-self.y[i-1])**2
+            self.d_list.append(self.traverse_dist)
+    
+    def get_curvature_direction(self):
         pass
+
+    def resample_by_dist(self):
+        pass
+
+    def enquire(self,dist):
+        pass
+
+    def visualize_map(self):
+        plt.scatter(self.x,self.y,c=self.height,s=0.5)
+        plt.colorbar()
+        plt.show()
     
 
-# %% 读取地图数据
 route = pd.read_csv('codes\maps\Tokyo.csv')
 route = route.transpose().to_numpy()
-y,x = longitudeLatitude_2_xy(route[0],route[1])
-h = route[2]
-plt.scatter(x,y,c='red',s=3)
-y,x,h = resample([y,x,h])
 
-coordinate_2_dist(x,y)
-# %% 地图可视化
-plt.scatter(x,y,c=h,s=0.5)
-plt.colorbar()
-plt.show()
+t = track(route)
